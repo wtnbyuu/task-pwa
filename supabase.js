104fab454a01d047b66e0ca484234e3aa8bfad52
yuwatanabe <watanabe@nn-i-nn.com>
2026-03-29 20:56:49 +0900

    fix: add Supabase credentials

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
-const SUPABASE_URL = 'YOUR_URL'       // 例: https://xxxx.supabase.co
-const SUPABASE_ANON_KEY = 'YOUR_ANON_KEY'
+const SUPABASE_URL = 'https://glngwocguhzsunsoeoqb.supabase.co'       // 例: https://xxxx.supabase.co
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdsbmd3b2NndWh6c3Vuc29lb3FiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3ODIyMTMsImV4cCI6MjA5MDM1ODIxM30.AMCuxMncE4DFJfAkX_dBc8gifOdQM5ZBjtdodogikoQ'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
